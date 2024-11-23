89d9975e2fd024d0b4cd45a850fcf28b647c03b7
Abdulrehman-8 <abdulrehmana3478@gmail.com>
2024-11-23 11:48:29 +0500

    Added decrease count functionality

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ increaseCount.addEventListener( 'click', (e) => {
     console.log(count);
     countAdder.textContent = `Click to increase your count: ${count++}`;
 })
+let decreaseCount = document.getElementById('decreasingButton');
+decreaseCount.addEventListener( 'click', (e) => {
+    console.log(count) 
+    countAdder.textContent = `Click to decrease your count: ${count--}`;
+})
