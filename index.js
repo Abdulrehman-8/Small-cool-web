4e4f8ded7cc3bf634642ec6154ac4acafc41fece
Abdulrehman-8 <abdulrehmana3478@gmail.com>
2024-11-23 12:06:11 +0500

    Completed all the counts

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 let countAdder = document.getElementById("incCount");
 let increaseCount = document.getElementById("countingButton");
 let count = 0;
+function wowChecker() {
+    if (count === 27 || count === 52 || count === -27 || count === -52) {
+        prompt("You've been counting a lot.. Much Wow!");
+       }
+    }
 increaseCount.addEventListener( 'click', (e) => {
     console.log(count);
     countAdder.textContent = `Click to increase your count: ${count++}`;
+    wowChecker();
 })
 let decreaseCount = document.getElementById('decreasingButton');
 decreaseCount.addEventListener( 'click', (e) => {
     console.log(count) 
     countAdder.textContent = `Click to decrease your count: ${count--}`;
+    wowChecker()
+})
+let ultimateResetter = document.getElementById('resetCount');
+
+ultimateResetter.addEventListener('click', (e) => {
+    count = 0;
+    console.log(count);
+    countAdder.textContent = `Click to increase your count: ${count}`
 })
